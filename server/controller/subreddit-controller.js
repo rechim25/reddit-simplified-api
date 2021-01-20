8db613dd3bb343c09c4535a0ad67669ee4ff409a
rechim25 <radu.echim.19@ucl.ac.uk>
2021-01-20 22:12:51 +0200

    feat: Throw error when subreddit has not content

--- server/controller/subreddit-controller.js
+++ server/controller/subreddit-controller.js
@@ -1,36 +1,39 @@
-import { response } from 'express';
-import ApiError from '../error/ApiError.js';
-import { fetchTopPostsFromSubreddit } from '../util/apiUtil.js';
-import  {filterSubredditListingData } from '../util/apiUtil.js';
+import { response } from "express";
+import ApiError from "../error/ApiError.js";
+import { fetchTopPostsFromSubreddit } from "../util/apiUtil.js";
+import { filterSubredditListingData } from "../util/apiUtil.js";
 
 export const getTopPostsFromSubreddit = (req, res, next) => {
-    if (subreddit === null) {
-        next(ApiError.badRequest(`Query parameter ${subreddit} cannot be null`));
+  if (subreddit === null) {
+    next(ApiError.badRequest(`Query parameter ${subreddit} cannot be null`));
+    return;
+  }
+  var subreddit = req.params.subreddit;
+  var timeframe = req.query.timeframe;
+  var limit = req.query.limit ? req.query.limit : 25;
+  fetchTopPostsFromSubreddit(subreddit, timeframe, limit)
+    .then((response) => {
+      if (response.data.data && !response.data.data.dist) {
+        next(ApiError.badRequest(`Subreddit ${subreddit} does not exist`));
         return;
-    }
-    var subreddit = req.params.subreddit;
-    var timeframe = req.query.timeframe;
-    var limit = req.query.limit ? req.query.limit : 25;
-    fetchTopPostsFromSubreddit(subreddit, timeframe, limit)
-        .then(response => {
-            return res.json(filterSubredditListingData(response.data));
-        })
-        .catch(error => {
-            console.log(error);
-            if (error.response) {
-                if (error.response.status == 403) {
-                    next(ApiError.badRequest(`Subreddit ${subreddit} is private`));
-                    return;
-                }
-                if (error.response.status == 404) {
-                    next(ApiError.badRequest(`Subreddit ${subreddit} does not exist`));
-                    return;
-                }
-                next(ApiError.internalError("Request failed..."));
-            }
-        });
-}
+      }
+      return res.json(filterSubredditListingData(response.data));
+    })
+    .catch((error) => {
+      if (error.response) {
+        if (error.response.status == 403) {
+          next(ApiError.badRequest(`Subreddit ${subreddit} is private`));
+          return;
+        }
+        if (error.response.status == 404) {
+          next(ApiError.badRequest(`Subreddit ${subreddit} does not exist`));
+          return;
+        }
+        next(ApiError.internalError("Request failed..."));
+      }
+    });
+};
 
 export const getHotPostsFromSubreddit = (req, res, next) => {
-    res.send("Get hot posts from subreddit");
-}
+  res.send("Get hot posts from subreddit");
+};
--- server/error/api-error-handler.js
+++ server/error/api-error-handler.js
@@ -1,11 +1,9 @@
-import ApiError from './ApiError.js'
+import ApiError from "./ApiError.js";
 
 export default function apiErrorHandler(err, req, res, next) {
-    console.error(err);
-    if (err instanceof ApiError) {
-        res.status(err.code).json(err.message);
-        return;
-    } 
-    res.status(500).json();
-
+  if (err instanceof ApiError) {
+    res.status(err.code).json(err.message);
+    return;
+  }
+  res.status(500).json();
 }
